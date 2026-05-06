8b380f13e935ef519766a9bd2e42909f1bb978d6
janmejayananda955 <janmejayananda955@gmail.com>
2026-05-07 00:10:43 +0530

    backend fix isssue mongodb not connecting

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 import app from "./src/app.js"
 import dotenv from "dotenv";
-import dns from "node:dns";
 import connctDB from "./src/config/database.js";
 
 dotenv.config();
-dns.setDefaultResultOrder('ipv4first');
 connctDB(); 
  
 app.listen(3000,()=>{
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.route.js";
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173"
+}))
  
 /** require all the routs here */
 
